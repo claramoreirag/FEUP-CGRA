ffe3bc4377443aed98ba166d413d358cb4e4af65
claramoreirag <claramoreirag@users.noreply.github.com>
2020-05-10 20:23:28 +0100

    added files

---
+++ MyFlag.js
@@ -0,0 +1,67 @@
+
+/**
+ * MyFlag
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyFlag extends CGFobject {
+	constructor(scene) {
+        super(scene);
+        this.flag = new MyPlane(this.scene, 20);
+        
+        this.initTexture(this.scene);
+
+    }
+
+    initTexture(scene){
+        //texture
+        this.texture = new CGFappearance(this.scene);
+        this.texture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.texture.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.texture.setSpecular(0.0, 0.0, 0.0, 1);
+        this.texture.setShininess(10.0);
+        this.texture.loadTexture('images/flag1.png');
+        this.texture.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+        
+        //shaders
+        this.shaderback = new CGFshader(this.scene.gl, "shaders/flag.vert", "shaders/flag.frag");
+        this.shaderback.setUniformsValues({ uSampler1: 1 });
+        this.shaderback.setUniformsValues({ speedFactor: 0 });
+        this.shaderback.setUniformsValues({ timeFactor: 0 });
+
+        this.shaderfront=new CGFshader(this.scene.gl, "shaders/flagback.vert", "shaders/flag.frag");
+        this.shaderfront.setUniformsValues({ uSampler1: 1 });
+        this.shaderfront.setUniformsValues({ speed: 0 });
+        this.shaderfront.setUniformsValues({ timeFactor: 0 });
+    }
+    
+    update(t, v){
+        this.shaderfront.setUniformsValues({ timeFactor: t });
+        this.shaderfront.setUniformsValues({ speedFactor: v });
+
+        this.shaderback.setUniformsValues({ timeFactor: t});
+        this.shaderback.setUniformsValues({ speedFactor: v });
+    }
+	
+	display(){
+        
+        this.texture.apply();
+        this.scene.setActiveShader(this.shaderfront);
+        this.scene.pushMatrix();
+        this.scene.translate(0,1,-3);
+        this.scene.scale(0.05,0.7,1.5);
+        this.scene.rotate(Math.PI/2, 0,1,0);
+        this.flag.display();
+        this.scene.popMatrix();
+        this.scene.setActiveShader(this.scene.defaultShader);
+
+        this.scene.setActiveShader(this.shaderback);
+        this.scene.pushMatrix();
+        this.scene.translate(0,1,-3);
+        this.scene.scale(0.05,0.7,1.5);
+        this.scene.rotate(-Math.PI/2, 0,1,0);
+        this.flag.display();
+        this.scene.popMatrix();
+        this.scene.setActiveShader(this.scene.defaultShader);
+    }
+}
---
+++ shaders/flagback.vert
@@ -0,0 +1,29 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
+attribute vec2 aTextureCoord;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform mat4 uNMatrix;
+
+varying vec2 vTextureCoord;
+uniform sampler2D uSampler;
+
+uniform float timeFactor;
+uniform float speedFactor;
+
+
+void main() {
+    
+
+    vTextureCoord = aTextureCoord;
+
+    vec3 offset = vec3 (0.0,0.0,0.0);
+    //offset.z=0.07*sin(aVertexPosition.x*20.0);
+    offset.z=sin(vTextureCoord.s*20.0 + 3.0*timeFactor + 3.0*speedFactor);
+    gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition + offset, 1.0);
+} 
